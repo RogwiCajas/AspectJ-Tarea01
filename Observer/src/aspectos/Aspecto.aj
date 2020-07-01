3b5ecbfab3f331567566242032d5fd86c472c77e
walter santacruz <wasantac@espol.edu.ec>
2020-06-30 23:22:39 -0500

    Registo Cambio Color

--- Observer/src/aspectos/Aspecto.aj
+++ Observer/src/aspectos/Aspecto.aj
@@ -1,22 +1,33 @@
 package aspectos;
 
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBase;
 import javafx.stage.Stage;
 import prueba.Main;
 
 public aspect Aspecto {
-	pointcut CambioColor():call(void Main.start(Stage arg0));
+
+	pointcut cambiarColorB() :call(* updateB(..));
 	
-	after(): CambioColor(){
+	after() : cambiarColorB()  {
+		System.out.println("Se cambio a Azul");
 		
-		if (comprobar().equals("Azul")) {
-			Main.cambiarAzul();
-		}
+	}
+	
+	pointcut cambiarColorR() :call(* updateR(..));
+	
+	after() : cambiarColorR()  {
+		System.out.println("Se cambio a Rojo");
+		
+	}
+	pointcut cambiarColorG() :call(* updateG(..));
+	
+	after() : cambiarColorG()  {
+		System.out.println("Se cambio a Verde");
 		
 	}
 
-	public static String comprobar() {
-		
-		return "Azul";
-	}
 }
--- Observer/src/prueba/Main.java
+++ Observer/src/prueba/Main.java
@@ -2,9 +2,12 @@ package prueba;
 
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,17 +16,34 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage arg0) throws Exception {
+		VBox root = new VBox();
+		EventHandler<ActionEvent> evB = new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		        updateB(root);
+		    }
+		};
+		EventHandler<ActionEvent> evR = new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		        updateR(root);
+		    }
+		};
+		EventHandler<ActionEvent> evG = new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		        updateG(root);
+		    }
+		};
 		//Creo la ventana
 		Stage stage = new Stage();
-		VBox root = new VBox();
+		
 		//Creo los botones
 		Button Bluebt = new Button("Blue");
 		Button Redbt = new Button("Red");
 		Button Greenbt = new Button("Green");
 		//
-		Bluebt.setOnAction(e -> root.setStyle("-fx-background-color: Blue"));
-		Redbt.setOnAction(e -> root.setStyle("-fx-background-color: Red"));
-		Greenbt.setOnAction(e -> root.setStyle("-fx-background-color: Green"));
+		
+		Bluebt.setOnAction(evB);
+		Redbt.setOnAction(evR);
+		Greenbt.setOnAction(evG);
 		//
 		root.getChildren().addAll(Bluebt,Redbt,Greenbt);
 		root.setAlignment(Pos.CENTER);
@@ -33,6 +53,19 @@ public class Main extends Application {
 	}
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public void updateB(VBox root) {
+		root.setStyle("-fx-background-color: Blue");
+		
+	}
+	public void updateR(VBox root) {
+		root.setStyle("-fx-background-color: Red");
+		
+	}
+	public void updateG(VBox root) {
+		root.setStyle("-fx-background-color: Green");
+		
 	}
 
 }
